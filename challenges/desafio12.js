32ba07eccc2cf1dd88d24d1e42f4ddcd7d1ea42c
pand-oly <hoa98hoa@gmail.com>
2022-10-17 19:09:14 -0300

    Adjust lint

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,9 +1,9 @@
 db.produtos.updateMany(
   { $and: [
-    { ingredientes: {$not: { $in: ["ketchup"] } } },
+    { ingredientes: { $not: { $in: ["ketchup"] } } },
     { nome: { $not: { $eq: "McChicken" } } },
   ] },
-  { $addToSet: { ingredientes: "ketchup" } }
+  { $addToSet: { ingredientes: "ketchup" } },
 );
 
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
