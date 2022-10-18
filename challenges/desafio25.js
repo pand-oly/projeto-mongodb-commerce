e2c5c07676aa27be572e8798189e5c3afe2582c8
pand-oly <hoa98hoa@gmail.com>
2022-10-18 10:31:20 -0300

    lint desafios 24, 25, 26

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -3,10 +3,10 @@ db.produtos.updateMany(
     $elemMatch: {
       tipo: "sódio", percentual: { $gte: 40 },
     },
-  }},
+  } },
   { $addToSet: { tags: "muito sódio" } },
 );
 
 db.produtos.find({},
-  { _id: 0, nome: 1, tags: 1 }
+  { _id: 0, nome: 1, tags: 1 },
 );
--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -3,10 +3,10 @@ db.produtos.updateMany(
     $elemMatch: {
       tipo: "sódio", percentual: { $gt: 20, $lt: 40 },
     },
-  }},
+  } },
   { $addToSet: { tags: "contém sódio" } },
 );
 
 db.produtos.find({},
-  { _id: 0, nome: 1, tags: 1 }
+  { _id: 0, nome: 1, tags: 1 },
 );
